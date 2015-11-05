24d2408b95e2f35e9793c8ade5fa5d7d374b6459
Andrew Hayward <alc.hayward@gmail.com>
2015-11-05 20:14:49 +1100

    fixed more bugs

--- PfaffianStates/make_betas.m
+++ PfaffianStates/make_betas.m
@@ -1,9 +1,10 @@
 addpath('../exact/','../exact/systems')
 nParticles = 4;
 lattice = [4,4];
-delta = -20;
+delta = -10;
+betas = 0:.05:2; % about 40 points
 beta_confs = Generate_Pfaffian_beta2_Configs(...
-    0:.1:2,cellfun(@(ld)new_lattice_config(ld,nParticles),...
+    betas,cellfun(@(ld)new_lattice_config(ld,nParticles),...
     {lattice},'UniformOutput',0),delta);
 confs = Get_Config_Data(beta_confs);
 filename = ['beta_data', arrayfun(@(x)num2str(x),[lattice(1),lattice(2),nParticles,delta])];
