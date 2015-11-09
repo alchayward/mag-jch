fc30fb2505cdff3e383ec6e71a11d6f2e57b49e1
Andrew Hayward <alc.hayward@gmail.com>
2015-11-09 13:06:42 +1100

    added Laughlin state computations

---
+++ LaughlinStates/make_detunings.m
@@ -0,0 +1,10 @@
+detunings = -10:0.5:10;
+lattice = [4,4]; 
+nParticles = 4;
+addpath('../exact/','../exact/systems')
+de_confs = Generate_Pfaffian_Detuning_Configs(...
+    detunings,cellfun(@(ld)new_lattice_config(ld,nParticles),...
+    {lattice},'UniformOutput',0));
+confs = Get_Config_Data(de_confs);
+filename = ['delta_data', arrayfun(@(x)num2str(x),[lattice(1),lattice(2),nParticles])];
+save(filename,'confs');
---
+++ LaughlinStates/new_lattice_config.m
@@ -0,0 +1,3 @@
+function conf = new_lattice_config(lattice_dims,nParticles)
+    conf = struct('lattice_dims',lattice_dims,'nParticles',nParticles);
+end
